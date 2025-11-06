87031f1c6e839934908708dc54f3920c41aaa00f
Pokehero77 <146128847+Pokehero77@users.noreply.github.com>
2025-11-06 12:59:40 -0500

    removed catagory functionality

--- main.py
+++ main.py
@@ -9,22 +9,21 @@ def load_transactions():
     with open(FILENAME, newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader, None) #skip
-        return [{"amount": float(a), "category": c, "note": n} for a, c, n in reader]
+        return [{"amount": float(a), "note": n} for a, n in reader]
 
 def save_transactions(transactions):
     with open(FILENAME, "w", newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(["amount", "category", "note"])
+        writer.writerow(["amount", "note"])
         for t in transactions:
-            writer.writerow([t["amount"], t["category"], t["note"]])
+            writer.writerow([t["amount"], t["note"]])
     print(f"Saved to {FILENAME}")
 
 def add_transaction(transactions):
     try:
         amount = float(input("Amount (+income, -expense): "))
-        category = input("Category: ").strip()
         note = input("Note: ").strip()
-        transactions.append({"amount": amount, "category": category, "note": note})
+        transactions.append({"amount": amount, "note": note})
         print("Added")
     except ValueError:
         print("Invalid amount.")
@@ -34,10 +33,10 @@ def view_transactions(transactions):
         print("No transactions.")
         return
     total = sum(t["amount"] for t in transactions)
-    print("\nAmount | Category | Note")
+    print("\nAmount | Note")
     print("-" * 40)
     for t in transactions:
-        print(f"{t['amount']:>7.2f} | {t['category']:<8} | {t['note']}")
+        print(f"{t['amount']:>7.2f} | {t['note']}")
     print("-" * 40)
     print(f"Total balance: ${total:.2f}")
 
